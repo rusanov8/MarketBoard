4ee005585fde1c7721fc9d7ad8724db7cfcd465a
Egor Rusanov <rusanov@MacBook-Air-Egor.local>
2023-11-13 23:54:40 +0700

    add users model and urls

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,16 +1,59 @@
+from enum import Enum
+
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.db import models
-from users.managers import UserManager
+from .managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
 
-class UserRoles:
-    # TODO закончите enum-класс для пользователя
-    pass
+class UserRoles(models.TextChoices, Enum):
+    USER = 'user', _('User')
+    ADMIN = 'admin', _('Admin')
 
 
 class User(AbstractBaseUser):
-    # TODO переопределение пользователя.
-    # TODO подробности также можно поискать в рекоммендациях к проекту
-    pass
+
+    objects = UserManager()
+
+    @property
+    def is_admin(self):
+        return self.role == UserRoles.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == UserRoles.USER
+
+    username = None
+
+    first_name = models.CharField(max_length=100, verbose_name=_('First name'))
+    last_name = models.CharField(max_length=100, verbose_name=_('Last name'))
+    email = models.EmailField(verbose_name=_('Email'), unique=True)
+    phone = PhoneNumberField(max_length=30, verbose_name=_('Phone number'), unique=True)
+    role = models.CharField(max_length=10, choices=UserRoles.choices,
+                            default=UserRoles.USER, verbose_name=_('Role'))
+    avatar = models.ImageField(upload_to='avatars/', verbose_name=_('Avatar'), blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', "role"]
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        db_table = 'users'
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+
--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -1,8 +1,18 @@
 from django.urls import include, path
 from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
-# TODO подключите UserViewSet из Djoser.views к нашим urls.py
-# TODO для этокого рекоммендуется использовать SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from .apps import UsersConfig
+
+app_name = UsersConfig.name
+
+users_router = SimpleRouter()
+
+users_router.register("users", UserViewSet, basename="users")
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("", include(users_router.urls)),
 ]
